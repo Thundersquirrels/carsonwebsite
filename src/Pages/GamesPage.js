2834392f7ffabecdf5c921f03023d435e322b196
Carson Cox <carsoncox@carson-macbook.lan>
2024-09-27 05:11:50 -0700

    Removed dungeon crawler so build would compile on cloudflare

--- src/Pages/GamesPage.js
+++ src/Pages/GamesPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import HUD from './HUD';
 import Chessboard from '../Games/Chess/Chessboard';
-import DungeonCrawler from '../Games/DungeonCrawler/DungeonCrawler';
+// import DungeonCrawler from '../Games/DungeonCrawler/DungeonCrawler';
 
 const content = (
   <div>
     <h1>Games</h1>
     <p>Here are some games I've made:</p>
-    <DungeonCrawler />
+    {/* <DungeonCrawler /> */}
     <Chessboard />
   </div>
 );
